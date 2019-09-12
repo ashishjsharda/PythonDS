e2e3798a8ccb9f53daacb06ecfabf37337e5f0ef
Ashish S <ashishjsharda@gmail.com>
2019-09-12 07:38:22 -0400

    Stack in Python
    Stack in Python

---
+++ Stack.py
@@ -0,0 +1,16 @@
+'''
+Created on Sep 7, 2019
+
+Stacks in Python
+@author: asharda
+'''
+
+stack=[]
+#Insert item in stack
+stack.insert(0, 6)
+print(stack)
+stack.insert(0, 5)
+#Print Content of Stack 
+print(stack)
+#Pop the element from stack
+print(stack.pop(0))

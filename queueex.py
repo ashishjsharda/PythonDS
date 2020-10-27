9cdc84f21c75052f701943557c404248a24f9742
Ashish S <ashishjsharda@gmail.com>
2020-10-27 07:10:32 -0400

    Create queueex.py

---
+++ queueex.py
@@ -0,0 +1,11 @@
+#Initialize a queue
+queue=[]
+queue.append("John")
+queue.append("Bob")
+queue.append("Dillon")
+queue.append("Roe")
+print(queue)
+#Pop an element from queue
+queue.pop(0)
+#Print Queue again
+print(queue)
